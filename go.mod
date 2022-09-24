a48c7b69d3ff87d06dca7312baf0ac114fbfaed6
alozxy <506717975@qq.com>
2022-09-24 09:07:42 +0000

    add support for udp mode in windows (beta)

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module trav-bittorrent
 
 go 1.19
+
+require github.com/alozxy/udp-forward v0.0.0-20220924084014-ce7debda77ed
--- ipt_windows.go
+++ ipt_windows.go
@@ -4,20 +4,33 @@ import (
 	"log"
 	"os/exec"
 	"strconv"
+
+	"github.com/alozxy/udp-forward"
 )
+
+var forwarder *forward.Forwarder
 
 func clear_rule_v4() {
 
 	if out, err := exec.Command("netsh", "interface", "portproxy", "reset").CombinedOutput(); err != nil {
 		log.Fatalln("netsh return a non-zero value while clearing ipv4 rules:", string(out))
 	}
+
+	forwarder.Close()
 }
 
 func set_rule_v4(external_port uint16) {
 	local_port := get_conf("local_port").(uint16)
 	src_ip := local_ip("1.1.1.1:443")
 
+	var err error
+
 	if out, err := exec.Command("netsh", "interface", "portproxy", "add", "v4tov4", "listenport="+strconv.FormatUint(uint64(local_port), 10), "listenaddress="+src_ip.String(), "connectport="+strconv.FormatUint(uint64(external_port), 10), "connectaddress=127.0.0.1").CombinedOutput(); err != nil {
 		log.Fatalln("netsh return a non-zero value while setting ipv4 rules:", string(out))
+	}
+
+	forwarder, err = forward.Forward("0.0.0.0:"+strconv.FormatUint(uint64(local_port), 10), "127.0.0.1:"+strconv.FormatUint(uint64(external_port), 10), forward.DefaultTimeout)
+	if err != nil {
+		panic(err)
 	}
 }
